b7612fcfaf5968a69ce3805ba8c580735eb7965c
Romeu Vieira <romeu.bizz@gmail.com>
2019-01-10 23:28:33 +0000

    Bunch of random stuff because I switched IDE...

--- src/main.cpp
+++ src/main.cpp
@@ -23,139 +23,141 @@ constexpr float W = 1.00f;
 
 void win_game()
 {
-	ImGui::Begin("Game Window");
-	ImVec2 p = ImGui::GetCursorScreenPos();
-	ImGui::GetWindowDrawList()->AddRectFilled(p, ImVec2(p.x + 100, p.y + 100), ImGui::GetColorU32((ImGuiCol) 1));
-	ImGui::End();
+    ImGui::Begin("Game Window");
+    ImVec2 p = ImGui::GetCursorScreenPos();
+    ImGui::GetWindowDrawList()->AddRectFilled(p, ImVec2(p.x + 100, p.y + 100), ImGui::GetColorU32((ImGuiCol) 1));
+    ImGui::End();
 }
 
 void win_hex_editor(std::array<std::uint8_t, Constants::CH8_MEMORY_SIZE>& memory)
 {
-	static MemoryEditor mem_edit;
-	mem_edit.DrawWindow("Memory Editor", &memory, Constants::CH8_MEMORY_SIZE);
+    static MemoryEditor mem_edit;
+    mem_edit.DrawWindow("Memory Editor", &memory, Constants::CH8_MEMORY_SIZE);
 }
 
 int main(int argc, char** argv)
 {
-	// check arguments
-	if (argc < 2) {
-		return EXIT_FAILURE;
-	}
+    // check arguments
+    if (argc < 2) {
+        return EXIT_FAILURE;
+    }
 
-	// tests
-	if (std::strcmp(argv[1], "tests") == 0) {
-		// testing here
+    // tests
+    if (std::strcmp(argv[1], "tests") == 0) {
+        // testing here
 
-		// read file
-		std::ifstream f(argv[2], std::ios::binary);
-		std::vector<char> v(std::istreambuf_iterator<char>{f}, {});
+        // read file
+        std::ifstream f(argv[2], std::ios::binary);
+        std::vector<char> v(std::istreambuf_iterator<char>{f}, {});
 
-		chip8 a;
-		a.load_rom(std::move(v));
-		a.cycle();
-		return EXIT_SUCCESS;
-	}
+        chip8 a;
+        a.load_rom(std::move(v));
+        a.cycle();
+        return EXIT_SUCCESS;
+    }
 
-	// init chip8
-	std::ifstream f(argv[1], std::ios::binary);
-	std::vector<char> v(std::istreambuf_iterator<char>{f}, {});
+    // init chip8
+    std::ifstream f(argv[1], std::ios::binary);
+    std::vector<char> v(std::istreambuf_iterator<char>{f}, {});
 
-	chip8 a;
-	a.load_rom(std::move(v));
-	//a.cycle();
+    chip8 a;
+    a.load_rom(std::move(v));
 
-	// Setup SDL
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
-		printf("Error: %s\n", SDL_GetError());
-		return EXIT_FAILURE;
-	}
+    // Setup SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+        printf("Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-	SDL_DisplayMode current;
-	SDL_GetCurrentDisplayMode(0, &current);
-	SDL_Window* window = SDL_CreateWindow("Chip8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-										  1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    SDL_DisplayMode current;
+    SDL_GetCurrentDisplayMode(0, &current);
+    SDL_Window* window = SDL_CreateWindow("Chip8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
-	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-	SDL_GL_SetSwapInterval(1);
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_SetSwapInterval(1);
 
-	bool err = gl3wInit() != 0;
-	if (err) {
-		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
-		return EXIT_FAILURE;
-	}
+    bool err = gl3wInit() != 0;
+    if (err) {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return EXIT_FAILURE;
+    }
 
-	// Setup Dear ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	(void) io;
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void) io;
 
-	// imgui style
-	ImGui::StyleColorsDark();
+    // imgui style
+    ImGui::StyleColorsDark();
 
-	// Setup Platform/Renderer bindings
-	ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-	ImGui_ImplOpenGL3_Init(Constants::GLSL_VERSION);
+    // Setup Platform/Renderer bindings
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(Constants::GLSL_VERSION);
 
-	// Main loop
-	bool done = false;
-	while (!done) {
-		SDL_Event event;
+    // Main loop
+    bool done = false;
+    while (!done) {
+        SDL_Event event;
 
-		while (SDL_PollEvent(&event)) {
-			ImGui_ImplSDL2_ProcessEvent(&event);
-			if (event.type == SDL_QUIT) {
-				done = true;
-			}
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT) {
+                done = true;
+            }
 
-			if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE
-					&& event.window.windowID == SDL_GetWindowID(window)) {
-				done = true;
-			}
-		}
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE
+                    && event.window.windowID == SDL_GetWindowID(window)) {
+                done = true;
+            }
+        }
 
-		// Start the Dear ImGui frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplSDL2_NewFrame(window);
-		ImGui::NewFrame();
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame(window);
+        ImGui::NewFrame();
 
-		// insert windows here
-		win_game();
+        // insert windows here
+        win_game();
 
-		// hex editor
-		win_hex_editor(a.memory);
+        // hex editor
+        win_hex_editor(a.memory);
 
-		// chip8 cycle
-		a.cycle();
+        // demo window
+        ImGui::ShowDemoWindow();
 
-		// Rendering
-		ImGui::Render();
+        // chip8 cycle
+        a.cycle();
 
-		SDL_GL_MakeCurrent(window, gl_context);
-		glViewport(0, 0, (int) io.DisplaySize.x, (int) io.DisplaySize.y);
-		glClearColor(Constants::CLEAR_COLOR::X, Constants::CLEAR_COLOR::Y, Constants::CLEAR_COLOR::Z, Constants::CLEAR_COLOR::W);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		SDL_GL_SwapWindow(window);
-	}
+        // Rendering
+        ImGui::Render();
 
-	// Cleanup
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplSDL2_Shutdown();
-	ImGui::DestroyContext();
+        SDL_GL_MakeCurrent(window, gl_context);
+        glViewport(0, 0, (int) io.DisplaySize.x, (int) io.DisplaySize.y);
+        glClearColor(Constants::CLEAR_COLOR::X, Constants::CLEAR_COLOR::Y, Constants::CLEAR_COLOR::Z, Constants::CLEAR_COLOR::W);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
 
-	SDL_GL_DeleteContext(gl_context);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
 
-	return EXIT_SUCCESS;
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
